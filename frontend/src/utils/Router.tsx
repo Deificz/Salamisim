5cb821abf71b78060e029bf5f218664543095e31
Deificz <99456747+Deificz@users.noreply.github.com>
2023-10-25 17:15:07 +0800

    Changed Stack structure

--- frontend/src/utils/Router.tsx
+++ frontend/src/utils/Router.tsx
@@ -1,37 +1,35 @@
 import * as React from "react";
-
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import intro_1 from "../pages/intro/intro_1";
 import intro_2 from "../pages/intro/intro_2";
 import intro_3 from "../pages/intro/intro_3";
 
-type RootStackParamList = {
-  intro_1: undefined;
-  intro_2: undefined;
-  intro_3: undefined;
-};
 
-const RootStack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator();
 
 function Router() {
   return (
-    <RootStack.Navigator initialRouteName="intro_1">
-      <RootStack.Screen
-        name="intro_1"
-        component={intro_1}
-        options={{ headerShown: false }}
-      />
-      <RootStack.Screen
-        name="intro_2"
-        component={intro_2}
-        options={{ headerShown: false }}
-      />
-      <RootStack.Screen
-        name="intro_3"
-        component={intro_3}
-        options={{ headerShown: false }}
-      />
-    </RootStack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="intro_1">
+        <Stack.Screen
+          name="intro_1"
+          component={intro_1}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="intro_2"
+          component={intro_2}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="intro_3"
+          component={intro_3}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
