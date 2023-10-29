c70fba294543f325d274ecc48e77a9c27ebfd224
msllagas <mndyllagas@gmail.com>
2023-10-29 19:33:05 +0800

    create fake data for each models via seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Course;
+use App\Models\Student;
+use App\Models\University;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +23,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $universities = University::factory(5)->create();
+        $courses = Course::factory(5)->create();
+        $users = User::factory(3)->create();
+
+        $users->each(function ($user) use ($universities, $courses) {
+            Student::factory()->create([
+                'user_id' => $user->id,
+                'university_id' => $universities->random()->id,
+                'course_id' => $courses->random()->id,
+            ]);
+        });
     }
 }
