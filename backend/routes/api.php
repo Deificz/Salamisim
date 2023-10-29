36e1fedab264d58bec3f75b07ef6192190c48a56
msllagas <mndyllagas@gmail.com>
2023-10-29 19:34:21 +0800

    route for getting all students

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\StudentController;
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/students", [StudentController::class, 'index']);
